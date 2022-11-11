ec3c93649dc88a1f55797d509a0d1c845e6295ed
iminiscript <maninder@anattadesign.com>
2022-11-11 17:32:35 +0530

    Intial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Learning Goals from <b>14/11 to 22/11</b>
+    </h3>
+    <h4>PTO dates - <b>17/11 - 18/11</b></h4>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+h4 {
+  color: brown;
+  font-size: 1rem;
+  padding-top: 10px;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+b {
+  font-weight: bolder;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,44 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Fundamental Topics</template>
+   <ul>
+    <li>Vue CLI</li>
+    <li>Componets
+      <ul>
+        <li>Single File Component</li>
+        <li>Component Registratoion</li>
+        <li>Props</li>
+        <li>Events</li>
+        <li>Attributes</li>
+      </ul>
+    </li>
+    <li>Templates</li>
+    <li>Directives</li>
+    <li>API Styles
+      <ul>
+        <li>Options API</li>
+        <li>Composition API -  Basic</li>
+      </ul>
+    </li>
+    <li>App Configurations</li>
+    <li>Rendering List</li>
+    <li>Conditional Rendering</li>
+
+   </ul>
+    
+  </WelcomeItem>
+
+  
+</template>
